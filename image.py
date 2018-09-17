642ad46176df9ab1b567b7206f7594cee5ebffb5
ctmakro <ctmakro@gmail.com>
2018-09-17 15:46:20 +0800

    laplacian pyramid, lru_cache that works

--- image.py
+++ image.py
@@ -26,3 +26,68 @@ def artistic_enhance(image):
 
     hf = (image - blurred)*alpha+beta
     return np.clip(hf, 0, 1)
+
+def lru_cache(f):
+    d = {}
+    l = []
+
+    def hash_args(*a, **kw):
+        hash = '.'.join([str(id(i)) for i in a])
+        hash += '|'
+        hash += '?'.join([k+'%'+str(id(v)) for k,v in kw.items()])
+        return hash
+
+    def newf(*a, **kw):
+        hash = hash_args(*a, **kw)
+        if hash not in d:
+            d[hash] = f(*a,**kw)
+
+            l.append(hash)
+            if len(l)>50:
+                oldhash = l.popleft()
+                d.pop(oldhash, None)
+
+        return d[hash]
+    return newf
+
+@lru_cache
+def fib(n):
+    if n<=2:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+
+print(fib(50))
+
+@lru_cache
+def laplacian_pyramid(img, levels):
+    gaussian_after_downsize = [img]
+    laplacian = []
+
+    for i in range(levels):
+        orig = gaussian_after_downsize[-1]
+        blurred = cv2.GaussianBlur(
+            orig,
+            ksize=(0,0),
+            sigmaX=1.0,
+        )
+
+        laplacian.append(orig - blurred)
+
+        downsized = vis.resize_nearest(
+            blurred,
+            blurred.shape[0]//2, blurred.shape[1]//2)
+
+        gaussian_after_downsize.append(downsized)
+
+    laplacian.append(blurred)
+    return laplacian
+
+if __name__ == '__main__':
+    im = load_image('jeff.jpg')
+    im = np.divide(im, 255., dtype='float32')
+    lp = laplacian_pyramid(im, levels=4)
+    for id,i in enumerate(lp):
+        cv2.imshow(str(id), i)
+        print(id, i.shape, i.dtype, i.max(), i.min())
+    cv2.waitKey(0)
